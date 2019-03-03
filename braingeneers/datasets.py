29d674acabaadadbf279221eb8dc65ad03ead7fb
Rob Currie <rcurrie@ucsc.edu>
2019-03-03 12:36:25 -0800

    Better error messages when batch uuid or experiment path are not correct

--- braingeneers/datasets.py
+++ braingeneers/datasets.py
@@ -18,7 +18,12 @@ def load_batch(batch_uuid):
         UUID of batch of experiments within the Braingeneer's archive'
         Example: 2019-02-15, or d820d4a6-f59a-4565-bcd1-6469228e8e64
     """
-    return requests.get("{}/derived/{}/metadata.json".format(get_archive_url(), batch_uuid)).json()
+    r = requests.get("{}/derived/{}/metadata.json".format(get_archive_url(), batch_uuid))
+    if not r:
+        print("Unable to load {}, are you sure you have the correct batch uuid?".format(batch_uuid))
+        r.raise_for_status()
+    else:
+        return r.json()
 
 
 def load_experiment(path):
@@ -38,7 +43,12 @@ def load_experiment(path):
         All of the metadata associated with this batch
     """
     # Each experiment has a metadata file with all *.rhd headers and other sample info
-    return requests.get("{}/{}".format(get_archive_url(), path)).json()
+    r = requests.get("{}/{}".format(get_archive_url(), path))
+    if not r:
+        print("Unable to load {}, are you sure you have the correct experiment path?".format(path))
+        r.raise_for_status()
+    else:
+        return r.json()
 
 
 def load_blocks(metadata, start=0, stop=-1, step=1):
@@ -78,6 +88,7 @@ def load_blocks(metadata, start=0, stop=-1, step=1):
             for s in metadata["samples"][start:stop]], axis=1)
     else:
         # Load from PRP S3
+        print("loading from url")
         X = np.concatenate([
             np.load(np.DataSource(None).open("{}/{}".format(get_archive_url(), s["derived"]), "rb"))
             for s in metadata["samples"][start:stop]], axis=1)
