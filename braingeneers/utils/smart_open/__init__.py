871bf97ab3597916c50bc43142d696d577e413b4
davidparks21 <davidparks21@gmail.com>
2021-06-05 11:24:07 -0700

    Upgraded to smart_open 5.1.0 and tested that it still works. There were some version conflicts between 4.2.0 and 5.1.0

--- braingeneers/utils/smart_open/__init__.py
+++ braingeneers/utils/smart_open/__init__.py
@@ -1,14 +1,20 @@
 """
 Configures smart_open for braingeneers use on PRP/S3
+This replaces your python default `open` function with smart_open.open
+which supports both local and remote files.
 
 Usage example:
-    import braingeneers.utils.smart_open as smart_open
+    ```
+    from braingeneers.utils import smart_open
+
     with smart_open.open('s3://braingeneersdev/test_file.txt', 'r') as f:
         print(f.read())
+    ```
 """
 from smart_open import *
 import functools
 import os
+
 
 # noinspection PyShadowingBuiltins
 open = functools.partial(open, transport_params={
