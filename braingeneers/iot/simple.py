26c374228a79c6b6aef25913b48a43b48b631443
Matthew Elliott <melliot1@ucsc.edu>
2022-06-30 14:43:43 -0700

    udpated draw_schedule and added basic security feature to start_iot

--- braingeneers/iot/simple.py
+++ braingeneers/iot/simple.py
@@ -1,7 +1,5 @@
 """ A simple protocol for connecting devices to IoT and schedule experiments"""
 
-
-
 # import stuff
 from braingeneers.iot import messaging
 import uuid
@@ -11,12 +9,12 @@ import warnings
 import builtins
 import inspect
 import logging
+import traceback
 
 from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
 from datetime import date, datetime, timedelta
 from dateutil import tz
-
 
 
 def _updateIot(device_name, mb ):
@@ -36,32 +34,34 @@ def _updateIot(device_name, mb ):
         jobs_str.append( job_str.split("next run: ")[0]+"next run: "+next_time )# append
 
     # Update log
-    with open("iot.log") as file:
+    with open("iot.log") as file:                  # Get history of iot commands and scheduel from the file iot.log
         lines = file.readlines()
         log = [line.rstrip() for line in lines]
-
+        if len(log)>35:                            # If iot log is more than 25 lines, get just the last 25 lines
+            log = log[-35:]    
     mb.update_device_state( device_name, {"status":iot_status,"jobs":jobs,"schedule":jobs_str,"history":log} )
 
 
-    
-def start_iot(device_name, device_type, experiment):
+
+def start_iot(device_name, device_type, experiment, commands=[]): #, #allowed_commands={"pump":["func_1","func_1"], "image":["func_1","func_1"]}):
     """Create a device and have it start listening for commands. This is intended for simple use cases"""
     # Initialize environment
-    from braingeneers.iot import messaging; import uuid; import schedule; import time; import warnings; import logging #requried packages
+    from braingeneers.iot import messaging; import uuid; import schedule; import time; import warnings; import logging; import traceback #requried packages
     global iot_status                                                              # states if iot is running. Other iot functions can change it asynchronously
     warnings.filterwarnings(action='once')                                         # stops same warning from appearing more than once
     mb = messaging.MessageBroker(str(uuid.uuid4))                                  # spin up iot
     
     # Set up Logging
     open("iot.log", "w").close()
-    #open("all.log", "w").close() #logging.basicConfig(level=logging.WARNING, filename='all.log', filemode='w', format='%(asctime)s - %(message)s')
-    logging.basicConfig()
+    logging.basicConfig( level=logging.WARNING ) 
     logger = logging.getLogger('schedule')
     logger.setLevel(level=logging.DEBUG)
     f_handler = logging.FileHandler('iot.log')
     f_handler.setFormatter( logging.Formatter("%(asctime)s - %(message)s", "%Y-%m-%d %H:%M:%S %Z") ) # %(levelname)s
     logger.addHandler(f_handler)
-    
+    global print
+    print = logger.debug
+
     # Start IoT Device
     if device_name not in mb.list_devices_by_type( thingTypeName= device_type):    # check if device already exists
         mb.create_device( device_name= device_name, device_type= device_type)      # if not, create it
@@ -72,8 +72,15 @@ def start_iot(device_name, device_type, experiment):
     
     # Create function for when IoT command is received
     def respondToCommand(topic: str, message: dict):                               # build function that runs when device receives command
-        logger.debug(f"Run Command: {message['command']}")
-        exec(message["command"])                                                   # run python command that was sent
+        try:
+            if len(commands)>0:                                                    # if user inputed list of allowed commands 
+                if not "global iot_status; iot_status=" in message["command"] and \ 
+                   not any(x in message["command"] for x in commands ):            # check if sent command contains an allowed command, if not throw error
+                    raise Exception(f"User message-- {message['command']}-- doesn't contain required commands -- {commands}")         
+            logger.debug(f"Run Command: {message['command']}")                     # log to history that the sent command was run
+            exec(message["command"])                                               # run python command that was sent
+        except Exception as e:
+            logger.error("\n"+traceback.format_exc())
         _updateIot(device_name, mb )                                               # update iot state, in case schedule/status changed
     mb.subscribe_message( f"devices/+/{device_name}", respondToCommand )           # start listening for new commands
     #mb.subscribe_message(f"devices/{device_type}/{device_name}",respondToCommand) 
@@ -88,13 +95,11 @@ def start_iot(device_name, device_type, experiment):
                 _updateIot(device_name, mb )                                       # schedule info has changed, so update IoT state
         time.sleep(.1)                                                             # wait a little to save cpu usage
     mb.shutdown()                                                                  # shutdown iot at the end.
-                                                                     # shutdown iot at the end.    
-
     
+     
 def ready_iot():
     """Save source code for start_iot function to a place where it can be executed by the user"""
     builtins.ready_iot = inspect.getsource(start_iot)
-    
     
 def send( device_name, command ):
     """Send a python script as a string which is then implemented by an IoT device. This is intended for simple use cases"""
@@ -102,15 +107,14 @@ def send( device_name, command ):
     mb = messaging.MessageBroker(str(uuid.uuid4))                             # spin up iot
     mb.publish_message( topic=f"devices/dummy/{device_name}", message={"command": command } )    # send command to listening device
     mb.shutdown()                                                             # shutdown iot
-
-
+    
 def get_schedule( device_name ):
     """Get a list of scheduled commands from a device's shadow. This is intended for simple use cases"""
     warnings.filterwarnings("ignore")
     mb = messaging.MessageBroker(str(uuid.uuid4))                             # spin up iot
     my_schedule = mb.get_device_state( device_name )["schedule"]              # get schedule for device
     mb.shutdown()                                                             # shutdown iot
-    return my_schedule                                                         # return schedule to user
+    return my_schedule                                                        # return schedule to user
 
 
 def get_status( device_name ):
@@ -121,6 +125,14 @@ def get_status( device_name ):
     mb.shutdown()                                                        # shutdown iot
     return status                                                        # return schedule to user
 
+def get_history( device_name ):
+    """Get a list of scheduled commands from a device's shadow. This is intended for simple use cases"""
+    warnings.filterwarnings("ignore")
+    mb = messaging.MessageBroker(str(uuid.uuid4))                        # spin up iot
+    status = mb.get_device_state( device_name )["history"]                # get schedule for device
+    mb.shutdown()                                                        # shutdown iot
+    return status                                                        # return schedule to user
+
 def get_info( device_name ): 
     """Get public device info from its shadow. This is intended for simple use cases"""
     warnings.filterwarnings("ignore")
@@ -128,7 +140,6 @@ def get_info( device_name ):
     info = mb.get_device_state( device_name )                                 # get all info
     mb.shutdown()                                                             # shutdown iot
     return info                                                               # return info to user
-
 
 def draw_schedule( device_list ):
     """Draw a weekly schedule of all events that occure for a chose device or experiment"""
@@ -144,9 +155,8 @@ def draw_schedule( device_list ):
     ax.set_ylim( 24, 0)
     ax.set_yticks(range(0, 24))
     ax.set_yticklabels(["{0}:00".format(h) for h in range(0, 24)])
-    #plt.savefig('{0}.png'.format(os.path.splitext(sys.argv[1])[0]), dpi=200, bbox_inches='tight')
 
-    # !!! This code should be able to handle experiment types as well as device lists
+    # !!! This code should be able to handle experiment types 
     # Locally: schedule_str= [f"Job {i}: "+x.__repr__() for i,x in enumerate(schedule.get_jobs())]  #jobs= [job.__str__() for job in schedule.get_jobs()] 
     if type(device_list)==type(""):                    # if single device was passed to the function as a string instead of list, 
         device_list = [device_list]                    # turn it into a list
@@ -164,9 +174,8 @@ def draw_schedule( device_list ):
             if "UNTIL-" in jobs[i]:                                                           # if there is a specified stop time, use it
                 stop_time = datetime.fromisoformat( jobs[i].split("UNTIL-")[1] )
             else:                                                                             # otherwise stop at weekly cycle
-                today = (datetime.now() - timedelta(hours=7)).replace(hour=0,minute=0,second=0,microsecond=0) # WARNING: Check Daylight savings
-                stop_time =  today + timedelta(weeks=1)   # we will consider all job events that ocure in a week
-            job_times = []                   # create a list of all event times for a job
+                stop_time =  datetime.now() - timedelta(hours=7) + timedelta(weeks=1)     # Consider jobs that occur up to a week from now
+            job_times = []                                                                 # create a list of all event times for a job
             while next_run < stop_time:
                 job_times.append(next_run)
                 next_run += period
@@ -178,24 +187,28 @@ def draw_schedule( device_list ):
                 plt.fill_between([d, d + 0.96], [start, start], [end, end], color=colors[dev_num],alpha=0.3)
                 plt.text(d + 0.02, start + 0.02, '{0}:{1:0>2}'.format(event.hour, event.minute), va='top', fontsize=8)
                 plt.text(d + 0.48, start + 0.01, f"Job {i}", va='top', fontsize=8) #ha='center', va='center', fontsize=10)
+                
+    # Add a red line for when right now is
+    now =  datetime.now() - timedelta(hours=7) 
+    d = now.weekday() + 0.52                                                   # get day of week for event
+    start = float(now.hour) + float(now.minute) / 60                         # get start time of event
+    end = float(now.hour) + (float(now.minute)+5) / 60                      # Ends 15 minutes after start
+    plt.fill_between([d, d + 0.96 ], [start, start], [end, end], color='red' )
     plt.show()
 
-
     
-
-def shutdown( device_name ):
+def shutdown( device_name, hard=False ):
     """Stops iot listener on device by changing flag on shadow. This is intended for simple use cases"""
     send(device_name, "global iot_status; iot_status='shutdown'")
-    # the following lines of code may or may not be good to add, I haven't decided yet
-    #warnings.filterwarnings("ignore") #mb = messaging.MessageBroker(str(uuid.uuid4)) #mb.update_device_state( device_name, {"status":"shutdown"} ) #mb.shutdown()
-    
+    if hard == True:
+        mb = messaging.MessageBroker(str(uuid.uuid4))               # spin up iot
+        mb.update_device_state( device_name, {"status":"shutdown"} )              # change status flag on device state to run
+        mb.shutdown()   
     
 def pause( device_name ):
     """Pauses iot listener on device by changing flag on shadow. This is intended for simple use cases"""
-    send(device_name, "global iot_status; iot_status='pause'")
-
+    send(device_name, "global iot_status; iot_status='pause'")  
     
 def run( device_name ):
     """Resumes running of iot listener on device by changing flag on shadow. This is intended for simple use cases"""
     send(device_name, "global iot_status; iot_status='run'")
-
