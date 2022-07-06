840ed0d0c2dcf8a283790896916457bc001fb4d7
Matthew Elliott <melliot1@ucsc.edu>
2022-07-06 14:24:10 -0700

    embbedded _updateIot into start_iot

--- braingeneers/iot/simple.py
+++ braingeneers/iot/simple.py
@@ -17,34 +17,49 @@ from datetime import date, datetime, timedelta
 from dateutil import tz
 
 
-def _updateIot(device_name, mb ):
-    """Updates the IoT device's publicly visible state according to the local schedule/status"""
-    # Update schedule
-    jobs, jobs_str = [],[]
-    for i,job in enumerate(schedule.get_jobs()):                         # debugging: print(i) #print(job.__str__()) #print(job.__repr__())
-        jobs.append( job.__str__() )                                     # append python command that wass used to create shceduled job
-        if job.cancel_after:                                             # if job must be cancelled at a certain time, add cancel time
-            jobs[i]+= job.cancel_after.astimezone(tz.gettz('US/Pacific')).strftime("UNTIL-%Y-%m-%d %H:%M:%S") 
-            
-        job_str = f"Job {i}: "+job.__repr__()
-        if job.last_run:                                                                                # if job was previously run
-            last_time = job.last_run.astimezone(tz.gettz('US/Pacific')).strftime("%Y-%m-%d %H:%M:%S")   # get time of last scheduled event
-            job_str = job_str.split("last run: ")[0]+"last run: "+last_time+", next run: "
-        next_time = job.next_run.astimezone(tz.gettz('US/Pacific')).strftime("%Y-%m-%d %H:%M:%S)")      # get time of next scheduled event     
-        jobs_str.append( job_str.split("next run: ")[0]+"next run: "+next_time )# append
-
-    # Update log
-    with open("iot.log") as file:                  # Get history of iot commands and scheduel from the file iot.log
-        lines = file.readlines()
-        log = [line.rstrip() for line in lines]
-        if len(log)>35:                            # If iot log is more than 25 lines, get just the last 25 lines
-            log = log[-35:]    
-    mb.update_device_state( device_name, {"status":iot_status,"jobs":jobs,"schedule":jobs_str,"history":log} )
-
-
 
 def start_iot(device_name, device_type, experiment, commands=[]): #, #allowed_commands={"pump":["func_1","func_1"], "image":["func_1","func_1"]}):
     """Create a device and have it start listening for commands. This is intended for simple use cases"""
+    
+    # Run Helper Funcitons
+    def _updateIot(device_name, mb ):
+        """Updates the IoT device's publicly visible state according to the local schedule/status"""
+        # Update schedule
+        jobs, jobs_str = [],[]
+        for i,job in enumerate(schedule.get_jobs()):                         # debugging: print(i) #print(job.__str__()) #print(job.__repr__())
+            jobs.append( job.__str__() )                                     # append python command that wass used to create shceduled job
+            if job.cancel_after:                                             # if job must be cancelled at a certain time, add cancel time
+                jobs[i]+= job.cancel_after.astimezone(tz.gettz('US/Pacific')).strftime("UNTIL-%Y-%m-%d %H:%M:%S") 
+
+            job_str = f"Job {i}: "+job.__repr__()
+            if job.last_run:                                                                                # if job was previously run
+                last_time = job.last_run.astimezone(tz.gettz('US/Pacific')).strftime("%Y-%m-%d %H:%M:%S")   # get time of last scheduled event
+                job_str = job_str.split("last run: ")[0]+"last run: "+last_time+", next run: "
+            next_time = job.next_run.astimezone(tz.gettz('US/Pacific')).strftime("%Y-%m-%d %H:%M:%S)")      # get time of next scheduled event     
+            jobs_str.append( job_str.split("next run: ")[0]+"next run: "+next_time )# append
+
+        # Update log
+        with open("iot.log") as file:                  # Get history of iot commands and scheduel from the file iot.log
+            lines = file.readlines()
+            log = [line.rstrip() for line in lines]
+            if len(log)>35:                            # If iot log is more than 25 lines, get just the last 25 lines
+                log = log[-35:]    
+        mb.update_device_state( device_name, {"status":iot_status,"jobs":jobs,"schedule":jobs_str,"history":log} )
+
+    
+    # Create function for when IoT command is received
+    def respondToCommand(topic: str, message: dict):                               # build function that runs when device receives command
+        try:
+            if len(commands)>0:                                                    # if user inputed list of allowed commands 
+                if not "global iot_status; iot_status=" in message["command"] and not any(x in message["command"] for x in commands ):            # check if sent command contains an allowed command, if not throw error
+                    raise Exception(f"User message-- {message['command']}-- doesn't contain required commands -- {commands}")         
+            logger.debug(f"Run Command: {message['command']}")                     # log to history that the sent command was run
+            exec(message["command"])                                               # run python command that was sent
+        except Exception as e:
+            logger.error("\n"+traceback.format_exc())
+        _updateIot(device_name, mb )                                               # update iot state, in case schedule/status changed
+
+    
     # Initialize environment
     from braingeneers.iot import messaging; import uuid; import schedule; import time; import warnings; import logging; import traceback #requried packages
     global iot_status                                                              # states if iot is running. Other iot functions can change it asynchronously
@@ -69,21 +84,8 @@ def start_iot(device_name, device_type, experiment, commands=[]): #, #allowed_co
         assert "status" in mb.get_device_state(device_name), f"{device_name} has corrupted data! Talk to data team."
         assert mb.get_device_state(device_name)["status"]=="shutdown", f"{device_name} already exists and isn't shutdown. Please shutdown with 'iot.shutdown({device_name})'"
     mb.update_device_state( device_name, {"experiment":experiment,"status":"run","jobs":[],"schedule":[],"history":[]} ) # initialize iot state
-    
-    # Create function for when IoT command is received
-    def respondToCommand(topic: str, message: dict):                               # build function that runs when device receives command
-        try:
-            if len(commands)>0:                                                    # if user inputed list of allowed commands 
-                if not "global iot_status; iot_status=" in message["command"] and \
-                   not any(x in message["command"] for x in commands ):            # check if sent command contains an allowed command, if not throw error
-                    raise Exception(f"User message-- {message['command']}-- doesn't contain required commands -- {commands}")         
-            logger.debug(f"Run Command: {message['command']}")                     # log to history that the sent command was run
-            exec(message["command"])                                               # run python command that was sent
-        except Exception as e:
-            logger.error("\n"+traceback.format_exc())
-        _updateIot(device_name, mb )                                               # update iot state, in case schedule/status changed
     mb.subscribe_message( f"devices/+/{device_name}", respondToCommand )           # start listening for new commands
-    #mb.subscribe_message(f"devices/{device_type}/{device_name}",respondToCommand) 
+    #mb.subscribe_message(f"devices/{device_type}/{device_name}",respondToCommand)  
     
     # Perpetually listen for IoT commands
     iot_status = "run"                                                             # keep python running so that listener can do it's job
@@ -212,3 +214,4 @@ def pause( device_name ):
 def run( device_name ):
     """Resumes running of iot listener on device by changing flag on shadow. This is intended for simple use cases"""
     send(device_name, "global iot_status; iot_status='run'")
+
