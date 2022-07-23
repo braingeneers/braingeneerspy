b3f7a0adeaf34eb0610a5bb8a24a13a5ee18fd38
Matthew Elliott <melliot1@ucsc.edu>
2022-07-22 18:03:23 -0700

    Update __init__.py

--- braingeneers/analysis/__init__.py
+++ braingeneers/analysis/__init__.py
@@ -1,4 +1,5 @@
 import braingeneers
 from braingeneers.analysis.analysis import *
+from braingeneers.analysis.iot import *
 
 braingeneers.verify_optional_extras('analysis')  # verify the optional packages are installed
