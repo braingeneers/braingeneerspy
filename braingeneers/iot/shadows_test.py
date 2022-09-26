5dd73fb60f6613d4da7366b79c836e09b96b93fa
Pierre Baudin <pvbaudin@ucsc.edu>
2022-09-26 14:58:47 -0700

    added test script

--- braingeneers/iot/shadows_test.py
+++ braingeneers/iot/shadows_test.py
@@ -8,5 +8,40 @@ token = API_KEY
 # Create a shadow object
 instance = sh.DatabaseInteractor(endpoint, API_KEY)
 
-print(instance.create_interaction_thing("test_thingy", "BioPlateScope", "test_description", {"test_key": "test_value"}))
+thing = instance.Thing("BioPlateScope", "test_thing")
+
+print(thing.to_json())
+
+thing.add_to_shadow("fart","sparkle")
+
+print(thing.to_json())
+
+thing.add_to_shadow("barg","sparkleeness")
+thing.add_to_shadow("barg",{"nuts":"sparkle"})
+print(thing.to_json())
+# print(instance.create_interaction_thing("test_thingy", "BioPlateScope", "test_description", {"test_key": "test_value"}))
+
+'''
+How do we want this to flow
+
+Create interaction thing
+    - only happens once, when a new device is added to the system
+    - contains important imaging data
+
+Create Experiment
+    - happens when a new experiment is started
+    - Creates a plate or assigns existing plate to experiment
+    - assigns current experiment to interaction thing?
+
+Create Plate
+    - Plate spawns wells, wells don't exist outside of plates
+    - Plates have interaction things
+    - Imaging metadata should be transferred to the plate from the interaction thing
+
+Starting an image run
+    - Image uuid must be passed to plate object
+    - should not be able to start image run if interaction thing has no plate
+
+
+'''
 
