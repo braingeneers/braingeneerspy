ad2e8c6b8f3c564662da936faec88c980e4aa495
davidparks21 <davidparks21@gmail.com>
2023-02-15 16:31:30 -0800

    Changed minimum python to 3.8 to take advantage of dictionaries being ordered. Talk to Ash or DavidP

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='braingeneerspy',
     version='0.1.9',
-    python_requires='>=3.6.0',
+    python_requires='>=3.8.0',  # needed for ordered dictionaries
     description='Braingeneers Python utilities',
     long_description=long_description,
     long_description_content_type='text/markdown',
