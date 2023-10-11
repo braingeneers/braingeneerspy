2da15423bc6fdd9add12a6b534ca4b8525a52b66
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2023-10-11 13:18:23 -0700

    Update post_create.sh
    Update the wording for the comment
    
    Resolves https://github.com/braingeneers/braingeneerspy/pull/42#discussion_r1355718564

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,4 +1,4 @@
 # For writing commands that will be executed after the container is created
 
-# Installs `human_hip` as local library without resolving dependencies (--no-deps)
+# The given command installs a Python package located in the current directory in editable mode and includes all optional extras specified in the [all] section of braingeneers.
 python3 -m pip install -e ".[all]"
