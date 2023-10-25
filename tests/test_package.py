e01c43269e4789ca686f55a97b10a4a44bfc7a45
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2023-10-25 10:29:18 -0700

    Update test_package.py
    Remove the `_compat` module tests
    Part 2 of resolving comment https://github.com/braingeneers/braingeneerspy/pull/42#discussion_r1370772193

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,9 +6,3 @@ import braingeneers._compat.typing as typing_backports
 
 def test_version():
     assert m.__version__
-
-
-def test_has_typing():
-    assert hasattr(typing_backports, "TypeAlias")
-    assert hasattr(typing_backports, "Self")
-    assert hasattr(typing_backports, "assert_never")
