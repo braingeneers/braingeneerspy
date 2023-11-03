f1cea4a91981de4993aad1ef4595c7ec6f9ffc0a
Kate Voitiuk <kvoitiuk@ucsc.edu>
2023-11-03 10:14:21 -0700

    updated device code and added dependencies to configure.py

--- src/braingeneers/iot/example_device_main.py
+++ src/braingeneers/iot/example_device_main.py
@@ -8,10 +8,12 @@ if __name__ == "__main__":
     parser.add_argument('--device_name', type=str, required=False, default="spam", help='Name of device (default: spam)')
     parser.add_argument('--eggs', type=int, required=False, default=0, help='Starting quantity of eggs (default: 0)')
     parser.add_argument('--ham', type=int, required=False, default=0, help='Starting quantity of ham (default: 0)')
+    parser.add_argument('--spam', type=int, required=False, default=1, help='Starting quantity of spam (default: 1)')
+
     args = parser.parse_args()
 
     # Create a device object
-    device = ExampleDevice(device_name=args.device_name, eggs=args.eggs, ham=args.ham)
+    device = ExampleDevice(device_name=args.device_name, eggs=args.eggs, ham=args.ham, spam=args.spam)
 
     # Start the device activities, running in a loop
     # Control + C should gracefully stop execution
