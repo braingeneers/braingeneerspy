cd3f45cc6b845d9eef140f1a8110d7a721afcfa1
Lon Blauvelt <lblauvel@ucsc.edu>
2023-12-06 17:24:40 -0800

    Update _version.py

--- src/braingeneers/_version.py
+++ src/braingeneers/_version.py
@@ -1,2 +1,2 @@
-__version__ = version = '0.1.1.dev8'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev8')
+__version__ = version = '0.0.0.dev0'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev0')
