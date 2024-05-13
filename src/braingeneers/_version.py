ac5f9ec5ee9696330c1498c1ba635d238d52228d
DailyDreaming <lblauvel@ucsc.edu>
2024-05-13 11:50:36 -0700

    Update version 0.2.0 for release.

--- src/braingeneers/_version.py
+++ src/braingeneers/_version.py
@@ -1,2 +1,2 @@
-__version__ = version = '0.0.0.dev0'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev0')
+__version__ = version = '0.2.0.dev0'
+__version_tuple__ = version_tuple = (0, 2, 0, 'dev0')
